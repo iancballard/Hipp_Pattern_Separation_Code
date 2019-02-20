7e4280b85ec145b08526e73a7f74027a67e1cc2e
Ian Ballard <iancballard@gmail.com>
2019-02-20 13:08:26 -0800

    project level parameters for lyman

---
+++ project.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Configuration file for fd built on Tue Apr 28 14:34:29 2015
+
+# Default experiment name
+default_exp = 'sim'
+
+# Data directory is where the input data lives
+data_dir = 'data/'
+
+# Analysis directory is where anything written by these scripts will live
+analysis_dir = 'analysis'
+
+# Working directory is where data lives during workflow execution
+working_dir = 'analysis/workingdir'
+
+# Crash directory is where debugging info will be written if things go wrong
+crash_dir = '/home/iballard/fd/crashes/'
+
+# Set this to True to remove the working directory after each excecution
+rm_working_dir = False
+
+# Should normalization be performed using ANTS?
+ants_normalization = True
+
